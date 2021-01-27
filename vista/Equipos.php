faa337c765c1d499805148b4aa3742a03cf19b80
CristobalMH <rawrr.rasmus@gmail.com>
2021-01-27 16:59:59 -0600

    Se modificaron archivos para enlazarlos entre sí

---
+++ vista/Equipos.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Equipos</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<?php include 'Header.php'; ?>
+
+<body>
+  <br>
+  <div class="container">
+    <div class="row contenedor">
+      <div class="col-md-2">
+        <nav class="navbar2 navbar-light ">
+          <div class="container-fluid">
+            <form class="d-flex">
+              <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+      </div>
+
+      <div class="col-md-10">
+        <div class="col-md-12 table-scroll">
+          <table class="table caption-top table-hover">
+            <caption>Equipos
+              <button type="button align-right" class="btn btn-success" data-bs-toggle="modal"
+                data-bs-target="#exampleModal">
+                Añadir Equipo
+              </button>
+            </caption>
+            <thead>
+              <tr>
+                <th scope="col">#id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Foto</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </nav>
+  </div>
+</div>
+
+</body>
+<?php include 'Footer.php'; ?>
+</html>
---
+++ vista/PRESTAMOS.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prestamos</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../vista/css/stylesPrest.css">
+
+</head>
+<?php include 'Header.php'; ?>
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <div class="container">
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Prestamos</h1>
+        <div class="btn-toolbar mb-2 mb-md-0 ">
+        </div>
+      </div>
+      <div class="table-responsive table-scroll ">
+        <table class="table table-striped table-sm ">
+          <thead>
+            <tr>
+              <th>Id_Prestamo</th>
+              <th>Hora de inicio</th>
+              <th>Hora de entrega</th>
+              <th>01/01/2021</th>
+              <th>Estado</th>
+            </tr>
+          </thead>
+          <tbody >
+            <tr>
+              <td>0000_1</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+            <tr>
+              <td>0000_2</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0000_3</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0000_4</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+            <tr>
+              <td>0000_5</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0000_6</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+            <tr>
+              <td>0000_7</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0000_8</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+            <tr>
+              <td>0000_9</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0001_0</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+            <tr>
+              <td>0001_1</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0001_2</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+            <tr>
+              <td>0001_3</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+            <tr>
+              <td>0001_4</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0001_5</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>Finalizado</td>
+            </tr>
+            <tr>
+              <td>0001_6</td>
+              <td>12:00</td>
+              <td>14:00</td>
+              <td>01/01/2021</td>
+              <td>En curso</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  </div>
+
+</body>
+<?php include 'Footer.php'; ?>
+</html>
