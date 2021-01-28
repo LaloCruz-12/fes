016d31a524846f55b619f89e09268d0b289c2452
eduardordl97 <erdl5259@gmail.com>
2021-01-28 15:32:24 -0600

    se añade Usuarios.php

---
+++ vista/Usuarios.php
@@ -0,0 +1,264 @@
+<?php include('header.php'); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+<div class="row contenedor usuarios-main">
+    
+    <div class="col-md-2 ">	
+
+      <nav class="navbar2 navbar-light ">	
+        <div class="container-fluid">	
+          <form class="d-flex">	
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">	
+            <button class="btn btn-outline-success" type="submit">Search</button>	
+          </form>	
+        </div>	
+      </nav>	
+    </div>	
+    <div class="col-md-10 table-scroll">	
+      <table class="table caption-top table-hover">	
+        <caption>Usuarios	
+          <button type="button align-right" class="btn btn-success" data-bs-toggle="modal"	
+            data-bs-target="#exampleModal">	
+            Añadir Usuario	
+          </button>	
+        </caption>	
+        <thead>	
+          <tr>	
+            <th scope="col">#</th>	
+            <th scope="col">Nombre</th>	
+            <th scope="col">Apellido</th>	
+            <th scope="col">No.cuenta</th>	
+          </tr>	
+        </thead>	
+        <tbody>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>111111111</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">2</th>	
+            <td>Jacob</td>	
+            <td>Thornton</td>	
+            <td>@fat</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">3</th>	
+            <td>Larry</td>	
+            <td>the Bird</td>	
+            <td>@twitter</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">2</th>	
+            <td>Jacob</td>	
+            <td>Thornton</td>	
+            <td>@fat</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">3</th>	
+            <td>Larry</td>	
+            <td>the Bird</td>	
+            <td>@twitter</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">2</th>	
+            <td>Jacob</td>	
+            <td>Thornton</td>	
+            <td>@fat</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">3</th>	
+            <td>Larry</td>	
+            <td>the Bird</td>	
+            <td>@twitter</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">2</th>	
+            <td>Jacob</td>	
+            <td>Thornton</td>	
+            <td>@fat</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">3</th>	
+            <td>Larry</td>	
+            <td>the Bird</td>	
+            <td>@twitter</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">2</th>	
+            <td>Jacob</td>	
+            <td>Thornton</td>	
+            <td>@fat</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">3</th>	
+            <td>Larry</td>	
+            <td>the Bird</td>	
+            <td>@twitter</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">1</th>	
+            <td>Mark</td>	
+            <td>Otto</td>	
+            <td>@mdo</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">2</th>	
+            <td>Jacob</td>	
+            <td>Thornton</td>	
+            <td>@fat</td>	
+          </tr>	
+          <tr>	
+            <th scope="row">3</th>	
+            <td>Larry</td>	
+            <td>the Bird</td>	
+            <td>@twitter</td>	
+          </tr>	
+        </tbody>	
+      </table>	
+    </div>	
+</div>
+
+
+    <!-- Modal -->	
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">	
+      <div class="modal-dialog">	
+        <div class="modal-content">	
+          <div class="modal-header">	
+            <h5 class="modal-title" id="exampleModalLabel">Añadir Usuario</h5>	
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>	
+          </div>	
+          <div class="modal-body">	
+            <form >	
+              <fieldset class="Form-AddUser">	
+
+                <div>	
+
+                    <label></label>	
+                    <input class="controls" type="texts" placeholder="Nombre"> 	
+                </div>	
+
+                <div>	
+                    <label ></label>	
+                    <input class="controls" type="text" placeholder="Apellido paterno">	
+                </div>	
+
+                <div>	
+
+                    <label ></label>	
+                    <input class="controls" type="number" placeholder="Número de cuenta">	
+                </div>	
+                <div>	
+                  <label class="controls" >Carrera</label>	
+                  <select class="controls" name="genero">	
+
+                    <option>ICO</option>	
+
+                    <option>Derecho</option>	
+
+                  </select>	
+                </div>	
+                <div>	
+                  <label  ></label>	
+                  <input class="controls" type="password" placeholder="Contraseña">	
+                </div>	
+
+                <div>	
+                  <label class="controls" >Tipo de usuario</label>	
+                  <select class="controls">	
+
+                    <option >Alumno</option>	
+
+                    <option>Administrador</option>	
+
+                  </select>	
+                </div>	
+
+
+
+
+            </fieldset>	
+            </form>	
+          </div>	
+          <div class="modal-footer">	
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>	
+            <button type="button" class="btn btn-primary">Guardar</button>	
+          </div>	
+        </div>	
+      </div>	
+    </div>
+
+<?php include('footer.php'); ?>
